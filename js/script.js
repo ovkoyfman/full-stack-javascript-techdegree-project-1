fc7da237e89e5281b3cca9e24dbe02f47034eb96
Oksana Koyfman <oksanakoyfman@Oksanas-MacBook-Pro.local>
2020-04-16 20:30:17 -0400

    Updated code to satisfy extra credit requirements

--- js/script.js
+++ js/script.js
@@ -10,53 +10,117 @@ project 1 - A Random Quote Generator
  ***/
 const quotes = [{
 	quote: 'Only those who have learned the power of sincere and selfless contribution experience life’s deepest joy: true fulfillment.',
-	author: 'Tony Robbins'
+  author: 'Tony Robbins',
+  tags: ['inspirational','life','power','experiance']
 }, {
 	quote: 'This is my simple religion. There is no need for temples; no need for complicated philosophy. Our own brain, our own heart is our temple; the philosophy is kindness.',
 	author: 'Dalai Lama',
 	citation: '"The Dalai Lama: A Policy of Kindness" - book',
-	year: 1990
+  year: 1990,
+  tags: ['kindness','heart','simple','religion']
 }, {
 	quote: 'Believe you can and you\'re halfway there.',
-	author: 'Theodore Roosevelt'
+  author: 'Theodore Roosevelt',
+  tags: ['inspirational','you','believe','halfway']
 }, {
 	quote: 'Change is the law of life. And those who look only to the past or present are certain to miss the future.',
 	author: 'John F. Kennedy',
 	citation: 'Speach at City Hall in New Orleans, Louisiana',
-	year: 1963
+  year: 1963,
+  tags: ['change','life','future','past']
 }, {
 	quote: 'We can do anything we want to if we stick to it long enough.',
-	author: 'Helen Keller'
+  author: 'Helen Keller',
+  tags: ['long','enough','want','stick']
 }, {
 	quote: 'The future belongs to those who believe in the beauty of their dreams.',
-	author: 'Eleanor Roosevelt'
+  author: 'Eleanor Roosevelt',
+  tags: ['beauty','dreams','future','believe','who']
 }];
-/***
- * `getRandomQuote` function
- ***/
+
+let currentQuoteIndex;
+
+//function to return quote based on random number
 function getRandomQuote() {
-	const randomNumber = Math.floor(Math.random() * quotes.length);
-	return quotes[randomNumber];
+  let randomIndex;
+  //to prevent displaying the same quote twice in the row
+	do {
+    randomIndex = Math.floor(Math.random() * quotes.length);
+  }while(currentQuoteIndex === randomIndex);
+  currentQuoteIndex = randomIndex;
+  return quotes[randomIndex];
+   
 }
-/***
- * `printQuote` function
- ***/
+
+
+
+//function to display quote on the screen
 function printQuote() {
 	const quoteData = getRandomQuote();
 	const quoteP = document.querySelector('.quote');
 	const sourceP = document.querySelector('.source');
-	let sourceString = quoteData.author;
-	if (quoteData.citation) {
-		sourceString += `<span class="citation">${quoteData.citation}</span>`;
-	}
-	if (quoteData.year) {
-		sourceString += `<span class="year">${quoteData.year}</span>`;
-	}
-	quoteP.textContent = quoteData.quote;
-	sourceP.innerHTML = sourceString;
+  const quoteContainer = document.getElementById('quote-box');
+  let tagsString = '';
+  let tagContainer;
+
+  function printQuoteMainSection(){
+    let sourceString = quoteData.author;
+    //building citation string
+    if (quoteData.citation) {
+      sourceString += `<span class="citation">${quoteData.citation}</span>`;
+    }
+    if (quoteData.year) {
+      sourceString += `<span class="year">${quoteData.year}</span>`;
+    }
+    //updating quote text
+    quoteP.textContent = quoteData.quote;
+
+    //updating quote citation
+    sourceP.innerHTML = sourceString;
+  }
+
+  function appendTags(){
+    //checking if tags container doesn't exist then create one and attach to the quote container
+    if(!document.querySelector('.tags-container')){
+      tagContainer = document.createElement('div');
+      tagContainer.className = 'tags-container';
+      quoteContainer.appendChild(tagContainer);
+    }
+    //building tags string
+    for(let i = 0; i < quoteData.tags.length; i++){
+      tagsString += `<span class="tag">${quoteData.tags[i]}</span>`;
+    }
+    document.querySelector('.tags-container').innerHTML = tagsString;
+  }
+
+  function changeBodyColor(){
+    const body = document.getElementsByTagName('body');
+    //function to generate random color
+    function getRandomColor(){
+      let rgbColor = `RGB(`;
+      for(let i=0; i<3; i++){
+        const randomColorValue = Math.floor(Math.random()*256);
+        if(i === 2){
+          rgbColor += `${randomColorValue})`;
+        }else{
+          rgbColor += `${randomColorValue},`;
+        }
+      }
+      return rgbColor;
+    }
+    body[0].style.backgroundColor = getRandomColor();
+  }
+
+  printQuoteMainSection();
+  appendTags();
+  changeBodyColor();
 }
-/***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
- ***/
+
+//calling printQuote function to replace default quote on the page load 
+printQuote();
+
+//setting interval function to refresh quotes every 15 seconds
+setInterval(printQuote, 15000);
+
+//click event listener for the print quote button
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
